5b1d9066975b39b06e2c7155c03aebee4ac819ce
Sadi <sadimohammad7@gmail.com>
2020-11-11 00:14:00 +0600

    create tweet model

--- database/migrations/2020_11_10_180000_create_tweets_table.php
+++ database/migrations/2020_11_10_180000_create_tweets_table.php
@@ -15,7 +15,9 @@ class CreateTweetsTable extends Migration
     {
         Schema::create('tweets', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->integer('user_id');
+            $table->string('body')->nullable();
+            $table->dateTime('created_at');
         });
     }
 
